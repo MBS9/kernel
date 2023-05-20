fd74312108e024594f39f6a46804b091c11113f5
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-05-20 15:46:02 +0200

    x

--- lib/lib.c
+++ lib/lib.c
@@ -21,7 +21,7 @@ void *memcpy(void *dest, const void *src, size_t n) {
 }
  
 void *memset(void *s, int c, size_t n) {
-    uint8_t *p = (uint8_t *)s;
+    register uint8_t *p = (uint8_t *)s;
  
     for (size_t i = 0; i < n; i++) {
         p[i] = (uint8_t)c;
