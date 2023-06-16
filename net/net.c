ae48ece3fe327fbc5e15dca6624f22acab378489
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-06-16 16:11:47 +0200

    fix: code

--- net/net.c
+++ net/net.c
@@ -4,8 +4,13 @@
 
 uint32_t CSR_IO_MAPPED_BASE_ADDR;
 
+void writeOut(uint32_t adrr, uint32_t data) {
+    outportl(0xCF8, adrr);
+    outportl(0xCFC, data);
+}
+
 void nicAttach(uint16_t bus, uint16_t slot, uint16_t func) {
     CSR_IO_MAPPED_BASE_ADDR = pciConfigReadWord(bus, slot, func, INTEL_ETHER_CSR_IO_BASE_REG, PCI_SELECT_REGISTER);
-    outportl(CSR_IO_MAPPED_BASE_ADDR + 0x4, INTEL_ETHER_PORT_RESET);
+    writeOut(CSR_IO_MAPPED_BASE_ADDR, INTEL_ETHER_PORT_RESET);
     sleep(0x3FFFFF);
 }
