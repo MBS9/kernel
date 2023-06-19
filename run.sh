6d86e6ca26854db80d7cae435e2bd6894bd091b0
mbs9 <66876095+MBS9@users.noreply.github.com>
2023-06-19 14:13:33 +0200

    add ebugging

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -cdrom image.iso
+qemu-system-x86_64 -bios /usr/share/ovmf/OVMF.fd -cdrom image.iso -net nic,model=i82559er -net user -monitor stdio -s
